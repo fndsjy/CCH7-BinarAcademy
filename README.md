f8256d5166fb71c61a4a88e2d131bf9672082cbe
fndsjy <fendysanjaya40@gmail.com>
2022-11-04 20:31:05 +0700

    [Fendy] Converting to React and Making The Filtered Cars

---
+++ README.md
@@ -0,0 +1,15 @@
+# Getting Started with Create React App
+
+This project was bootstrapped with [Create React App].
+
+## Available Scripts
+
+In the project directory, run:
+
+### `yarn start`
+
+Runs the app in the development mode.\
+Open [http://localhost:3000](http://localhost:3000) to view it on your browser.
+
+The page will reload when you make changes.\
+You may also see any lint errors in the console.
---
+++ src/Components/SearchNotFound.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+const SearchNotFound = () => {
+    return (
+        <>
+            <Container fluid="md" className=" d-flex flex-column align-items-center justify-content-center">
+                <h1>Something's wrong here...</h1>
+                <p>We can't find the data you're looking for. Try again?</p>
+            </Container>
+        </>
+    );
+};
+
+export default SearchNotFound;
